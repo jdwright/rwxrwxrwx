79a478297316cb4184373c339c5495690129b267
Jonathan D. Wright <jdwright@ldc.upenn.edu>
2023-07-15 11:22:16 -0400

    skeleton

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,22 @@
 <script>
-    import 'virtual:windi.css'
+  // Your selected Skeleton theme:
+  import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
+
+  // This contains the bulk of Skeletons required styles:
+  import '@skeletonlabs/skeleton/styles/skeleton.css';
+
+  // Finally, your application's global stylesheet (sometimes labeled 'app.css')
+  import '../app.postcss';
+
+  import { AppShell, AppBar, LightSwitch } from '@skeletonlabs/skeleton';
 </script>
 
-<div class="c" style="padding: 50px 0 100px 0">
-    <slot />
-</div>
+<AppShell class="c" style="padding: 50px 0 100px 0">
+	<svelte:fragment slot="header">
+    <AppBar>
+      <svelte:fragment slot="lead">rwxrwxrwx</svelte:fragment>
+      <svelte:fragment slot="trail"><LightSwitch /></svelte:fragment>
+    </AppBar>
+  </svelte:fragment>
+  <slot />
+</AppShell>
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import WindiCSS from 'vite-plugin-windicss'
 
 export default defineConfig({
 	plugins: [
 		sveltekit(),
-	    WindiCSS(),
 	]
 });
